077f8819090391548989e0e9f691c2eaf9abcac3
webdevw <bec101rpamm@gmail.com>
2020-02-29 23:06:25 +0200

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -4,14 +4,37 @@ $(function(){
         rating: 5,
         starWidth: "12px",
         readOnly: true
-    })
+    });
 
     $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
-    })
+        slidesToScroll: 4,
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+        skin: "round",
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        grid: false,
+        prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+        $('.products__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.products__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
 
     var mixer = mixitup('.products__inner-box');
 
