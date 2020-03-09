ffe7c92972a492cb674e69cef4bfc1b68ef593e1
webdevw <bec101rpamm@gmail.com>
2020-03-09 23:18:13 +0200

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -36,7 +36,15 @@ $(function(){
         $('.icon-th-list').removeClass('active');
     });
 
-    var mixer = mixitup('.products__inner-box');
+    
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
 
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+    var mixer = mixitup('.products__inner-box');
 
 });
