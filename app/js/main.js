724c64d14e91e5742ece365dc81b837f63524c42
webdevw <bec101rpamm@gmail.com>
2020-03-15 22:51:40 +0200

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -28,13 +28,21 @@ $(function(){
         $('.products__item').addClass('list');
         $('.icon-th-list').addClass('active');
         $('.icon-th-large').removeClass('active');
-
     });
     $('.icon-th-large').on('click', function(){
         $('.products__item').removeClass('list');
         $('.icon-th-large').addClass('active');
         $('.icon-th-list').removeClass('active');
     });
+
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });        
 
     
     $('.menu__btn').on('click', function(){
