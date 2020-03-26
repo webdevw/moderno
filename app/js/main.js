53a241dc34000abc518c5012c489c92a8a16a267
webdevw <bec101rpamm@gmail.com>
2020-03-26 23:32:07 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,31 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1441,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 801,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
@@ -40,10 +65,10 @@ $(function(){
             $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
             $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
             $(this).addClass('active');
-            $('#'+id).addClass('active-tab').fadeIn();
+            $('#' + id).addClass('active-tab').fadeIn();
             return false;
         });        
-
+        $('input[type="file"], select').styler();
     
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
